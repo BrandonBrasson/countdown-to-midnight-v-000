b589a6f87b0f44804e408083b74217048cf5c180
Brandon Brasson <rocksong622@gmail.com>
2019-01-10 15:52:27 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,9 @@
 #write your code here
 
-def countdown
+def countdown(time)
+  while time > 0
+    puts "#{time} The Time Is Here"
+    time -= 1
+  end
+  return "OMG ITS HAPPENING GUYS OMG HAPPY NEW YEAR!A AAAAAA"
 end
