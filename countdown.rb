9c93f3c29161c8482731898b8b1f4df2550c6e68
Brandon Brasson <rocksong622@gmail.com>
2019-01-10 15:56:33 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,17 +8,10 @@ def countdown(time)
   return "HAPPY NEW YEAR!"
 end
 
-def countdown(seconds)
-  while seconds > 0
-    puts "#{seconds} SECOND(S)!"
-    seconds -= 1
-  end
-  return "HAPPY NEW YEAR!"
-end
 
-def countdown_with_sleep(seconds)
-  while seconds > 0
-    puts "#{seconds} SECOND(S)!"
+def countdown_with_sleep(time)
+  while time > 0
+    puts "#{time} SECOND(S)!"
     seconds -= 1
     sleep(1)
   end
